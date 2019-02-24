ee89897a9db52861e761dc6f081812652f4cbcc5
Sithifathimah <sithifathimah8@gmail.com>
2019-02-24 16:21:20 +0800

    Initial commit

---
+++ app/src/main/java/com/example/sithifathimah/firstapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.sithifathimah.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private Button btnNext;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnNext = (Button) findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenActivity2();
+            }
+        });
+    }
+
+    public void OpenActivity2() {
+        Intent intent = new Intent(MainActivity.this, SecondPage.class);
+        startActivity(intent);
+    }
+}
